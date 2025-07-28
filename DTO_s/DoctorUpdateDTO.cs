3a7211c2406b1e2f1ab9f5356c45e26eefa3a824
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:11:32 +0400

    Do the Doctor Update

---
+++ DTO_s/DoctorUpdateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+
+    public class DoctorUpdateDTO
+    {
+        public int DoctorId { get; set; }
+        public double Salary { get; set; }
+        public string Availability { get; set; }
+    }
+}
