2fbfe46e869b51405fa882a60f4015e3d4052384
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-07-28 15:46:41 +0400

    create department helper

---
+++ Helpers/DepartmentFileHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Codeline_HealthCareCenter_OOP.Models;
+
+namespace Codeline_HealthCareCenter_OOP.Helpers
+{
+    public static class DepartmentFileHelper
+    {
+        private static readonly string filePath = "departments.txt";
+
+        public static void SaveDepartments(List<Department> departments)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var dep in departments)
+                {
+                    writer.WriteLine($"{dep.DepartmentId}|{dep.DepartmentName}|{dep.IsActive}");
+                }
+            }
+        }
+
+        public static List<Department> LoadDepartments()
+        {
+            List<Department> departments = new();
+
+            if (!File.Exists(filePath))
+                return departments;
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    departments.Add(new Department
+                    {
+                        DepartmentId = int.Parse(parts[0]),
+                        DepartmentName = parts[1],
+                        IsActive = bool.Parse(parts[2])
+                    });
+                }
+            }
+
+            return departments;
+        }
+    }
+}
