23f77a0bf4b9bb60b1ebe7d963b34519551139f8
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 11:09:39 +0400

    add the boking out put dot

---
+++ DTO_s/BookingOutputDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class BookingOutputDTO
+    {
+        public int BookingId { get; set; }
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+        public string ClinicName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+    }
+
+}
