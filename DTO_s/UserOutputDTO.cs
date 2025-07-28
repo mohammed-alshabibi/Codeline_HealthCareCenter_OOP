82c9bcbe3c20ee280766dabf91835a4c9eb31016
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:10:30 +0400

    Do the User Output

---
+++ DTO_s/UserOutputDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class UserOutputDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
