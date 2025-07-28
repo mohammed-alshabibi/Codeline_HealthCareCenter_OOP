9bf0f4455bca6653b38a025bd22df8d433f00166
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 12:01:12 +0400

    add the DTO for the  Clini

---
+++ DTO_s/ClinicInputDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+ 
+        public class ClinicInputDTO
+        {
+            public string ClinicName { get; set; }
+            public string Department { get; set; }
+            public string Location { get; set; }
+        }
+    
+}
+
---
+++ DTO_s/ClinicOutputDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class ClinicOutputDTO
+    {
+        public int ClinicId { get; set; }
+        public string ClinicName { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+    }
+}
+
