32227c587d85de30205f7543c17e85d44882c6a3
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:10:50 +0400

    Do the Patient Input

---
+++ DTO_s/PatientInputDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class PatientInputDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string NationalID { get; set; }
+    }
+}
