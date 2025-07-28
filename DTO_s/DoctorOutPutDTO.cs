78696996657785e980c95a85cdd57bc6e78b3647
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:11:23 +0400

    Do the Doctor Output

---
+++ DTO_s/DoctorOutPutDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class DoctorOutPutDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Specialization { get; set; }
+        public string Availability { get; set; }
+        public double Salary { get; set; }
+    }
+}
