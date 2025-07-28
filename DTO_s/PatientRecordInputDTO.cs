6b9122df4ec59d7873f00c28ed45eeaedb09d7e8
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 12:14:07 +0400

    add the dto for the project of it

---
+++ DTO_s/PatientRecordInputDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class PatientRecordInputDTO
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
---
+++ DTO_s/PatientRecordOutputDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class PatientRecordOutputDTO
+    {
+        public int RecordId { get; set; }
+        public string PatientName { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
