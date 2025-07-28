dc03595f900fced1875040e5c760bfaf6d453a4b
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:09:37 +0400

    Do The User Input

---
+++ DTO_s/UserInputDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class UserInputDTO
+    {
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
