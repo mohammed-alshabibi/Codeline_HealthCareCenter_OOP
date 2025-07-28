54a5a208df0e6bd7f90276edd9b3a2780f4c2bb0
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:09:06 +0400

    Do Patient Class

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Codeline_HealthCareCenter_OOP.Models
 {
-    class Patient
+    public class Patient : User
     {
+        private string _phoneNumber;
+        private string _gender;
+        private int _age;
+        private string _nationalId;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        }
+
+        public string Gender
+        {
+            get => _gender;
+            set => _gender = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value > 0 ? value : 0;
+        }
+
+        public string NationalID
+        {
+            get => _nationalId;
+            internal set => _nationalId = value;
+        }
+
+        public Patient(string fullName, string email, string password, string phoneNumber, string gender, int age, string nationalId)
+            : base(fullName, email, password, "Patient")
+        {
+            PhoneNumber = phoneNumber;
+            Gender = gender;
+            Age = age;
+            NationalID = nationalId;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Phone: {PhoneNumber} | Age: {Age} | National ID: {NationalID}");
+        }
+
     }
 }
