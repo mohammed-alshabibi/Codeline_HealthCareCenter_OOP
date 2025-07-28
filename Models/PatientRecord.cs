fbbce178b7f25802e7b9a44c6a63466f08990b6c
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 12:11:15 +0400

    add the PatientRecord

--- Models/PatientRecord.cs
+++ Models/PatientRecord.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Codeline_HealthCareCenter_OOP.Models
 {
-    class PatientRecord
+    public class PatientRecord
     {
+        public int RecordId { get; set; }
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+        public DateTime VisitDate { get; set; }
     }
 }
