4f850e6b81d2edaf9e72610eee5b5f2b823c5d4b
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:11:03 +0400

    Do the Patient Output

---
+++ DTO_s/PatienoutputDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class PatienoutputDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string NationalID { get; set; }
+    }
+}
