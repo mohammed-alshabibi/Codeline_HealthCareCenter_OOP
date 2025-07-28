ae30103bdf3110063819176ebdbe372cc20cf23b
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 11:07:41 +0400

    add dto files

---
+++ DTO_s/BookingInputDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class BookingInputDTO
+    {
+        public int? ClinicId { get; set; }
+        public int? DepartmentId { get; set; }
+        public int? DoctorId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public TimeSpan AppointmentTime { get; set; }
+    }
+}
