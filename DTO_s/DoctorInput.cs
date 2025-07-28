0dc2843843118600005b93da57a04e59fe80fe25
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-07-28 15:11:14 +0400

    Do the Doctor Input

---
+++ DTO_s/DoctorInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeline_HealthCareCenter_OOP.DTO_s
+{
+    public class DoctorInput
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Specialization { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public int YearsOfExperience { get; set; }
+        public double Salary { get; set; }
+        public string Availability { get; set; }
+    }
+}
