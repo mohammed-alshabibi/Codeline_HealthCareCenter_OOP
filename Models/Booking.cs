3491028e0e9a1659e724257454556624098e5246
MOHAMMED <mohaalialalawi20@gmail.com>
2025-07-28 11:14:15 +0400

    add  the boking thimgs

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 using System;
 using System.IO;
 
-namespace Codeline_HealthCareCenter_OOP.Models
+namespace Codeline_HealthCareCenter_OOP.Models // Codeline_HealthCareCenter_OOP.Models
 {
-    public class Booking
+    public class Booking // Represents a booking in the healthcare system
     {
-
+        public int BookingId { get; set; } // Unique identifier for the booking
+        public int PatientId { get; set; } // Unique identifier for the patient
+        public int DoctorId { get; set; } // Unique identifier for the doctor
+        public int ClinicId { get; set; } // Unique identifier for the clinic
+        public int DepartmentId { get; set; } // Unique identifier for the department
+        public DateTime AppointmentDate { get; set; }   // Date of the appointment
+        public TimeSpan AppointmentTime { get; set; }   // Time of the appointment
     }
-}
